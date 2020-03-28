1ad2546ca649a25ba9fc73feca3ad311a99838fc
Amit Agrawal <amit.agrawal@pb.com>
2020-03-28 14:32:57 +0530

    First Revision- considering texts too for distinct computation

--- Task1.py
+++ Task1.py
@@ -10,11 +10,26 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
-dic = {}
-for call in calls:
-    record = Call(call)
-    dic[record.incoming] = 1
-    dic[record.answering] = 1
+# NOTE Set: to persist distinct values
+# - Methods: add(), update(), discard(), remove(), clear(), pop()
+# - discard() - does not throw error even if elem is not present in set.
+# - remove() - does not throw error when element does not exists in set.
+# - Ref link - https://www.programiz.com/python-programming/set
 
-print("There are <count> different telephone numbers in the records."
-      .replace("<count>", str(len(dic))))
+
+def find_unique_tele_numbers(texts, calls):
+    uniq_tele_nums = set({})     # using set instead of dict
+    for call in calls:
+        call = Call(call)
+        uniq_tele_nums.add(call.incoming)
+        uniq_tele_nums.add(call.answering)
+
+    for text in texts:
+        text = Text(text)
+        uniq_tele_nums.add(text.incoming)
+        uniq_tele_nums.add(text.answering)
+    return len(uniq_tele_nums)
+
+
+print("There are {0} different telephone numbers in the records.".format(
+    find_unique_tele_numbers(texts, calls)))
